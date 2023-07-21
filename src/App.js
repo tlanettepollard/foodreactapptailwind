decf7851c642a4dc5be3712bce14b786c684815c
T. Lanette <TLPollard4516@gmail.com>
2023-07-21 14:23:47 -0400

    Update: added Hero and other components
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
 
 function App() {
 	return (
 		<div>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
