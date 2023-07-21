cae9b826f7b79856435b7bc18384703e06a1d691
T. Lanette <TLPollard4516@gmail.com>
2023-07-21 13:28:39 -0400

    Adding Mobile Menu
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose } from 'react-icons/ai';
 import { BsFillCartFill } from 'react-icons/bs';
 
 const Navbar = () => {
@@ -34,10 +34,30 @@ const Navbar = () => {
 
 			{/* Cart Button */}
 
-			<button>
-				<BsFillCartFill />
+			<button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
+				<BsFillCartFill size={20} className='mr-2' />
 				Cart
 			</button>
+
+			{/* Mobile Menu */}
+			{/* Overlay */}
+			<div className='bg-black/80 fixed w-full h-screen z-10 top-0'></div>
+
+			{/* Side drawer Menu */}
+			<div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+				<AiOutlineClose
+					size={30}
+					className='absolute right-4 top-4 cursor-pointer'
+				/>
+				<h2 className='tex-2xl p-4'>
+					Best <span className='font-bold'>Eats</span>
+				</h2>
+				<nav>
+					<ul>
+						<li></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	);
 };
