3c784ce227d92919360604bcf74339bffe6d292e
T. Lanette <TLPollard4516@gmail.com>
2023-07-21 12:38:26 -0400

    Adding new repository
    Added new file to react app using tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+	return (
+		<div>
+			{/* Left Side */}
+			<div>
+				<div>
+					<AiOutlineMenu size='30' />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
