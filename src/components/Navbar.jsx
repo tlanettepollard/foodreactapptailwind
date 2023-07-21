89eae63ee938ffa4445ad04af1e318319088510f
T. Lanette <TLPollard4516@gmail.com>
2023-07-21 13:11:04 -0400

    Updates: Navbar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
+import { BsFillCartFill } from 'react-icons/bs';
 
 const Navbar = () => {
 	return (
@@ -14,11 +15,29 @@ const Navbar = () => {
 						Best <span className='font-bold'>Eats</span>
 					</h1>
 				</div>
-				<div>
-					<p>Delivery</p>
-					<p>Pickup</p>
+				<div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+					<p className='bg-black text-white rounded-full p-2'>Delivery</p>
+					<p className='p-2'>Pickup</p>
 				</div>
 			</div>
+
+			{/* Search Input */}
+
+			<div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+				<AiOutlineSearch size={20} />
+				<input
+					type='text'
+					placeholder='Search Foods'
+					className='bg-transparent p-2 w-full focus:outline-none'
+				/>
+			</div>
+
+			{/* Cart Button */}
+
+			<button>
+				<BsFillCartFill />
+				Cart
+			</button>
 		</div>
 	);
 };
