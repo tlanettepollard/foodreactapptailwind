321de353949b677df36c3f80b934b599886b9f0d
T. Lanette <TLPollard4516@gmail.com>
2023-07-21 12:50:41 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,20 @@ import { AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
 	return (
-		<div>
+		<div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
 			{/* Left Side */}
-			<div>
+			<div className='flex flex-row items-center'>
+				<div className='cursor-pointer'>
+					<AiOutlineMenu size={30} />
+				</div>
 				<div>
-					<AiOutlineMenu size='30' />
+					<h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+						Best <span className='font-bold'>Eats</span>
+					</h1>
+				</div>
+				<div>
+					<p>Delivery</p>
+					<p>Pickup</p>
 				</div>
 			</div>
 		</div>
